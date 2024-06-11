9fa86f95adc5aaf7617bd095ff4a2bb689c1e1d3
yyyujinnn <1209yujin@naver.com>
2024-06-11 15:36:41 +0900

    build: 프로젝트 생성

---
+++ src/test/java/SSOP/ssop/SsopApplicationTests.java
@@ -0,0 +1,13 @@
+package SSOP.ssop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
