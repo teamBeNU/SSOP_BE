c625cadae403ff5525f9b599fc94683282467fca
yyyujinnn <1209yujin@naver.com>
2024-08-02 23:31:09 +0900

    fix: TeamSpRepository 중복 선언 수정, @Repository 제거

--- src/main/java/SSOP/ssop/service/TeamSpService.java
+++ src/main/java/SSOP/ssop/service/TeamSpService.java
@@ -11,33 +11,31 @@ import java.util.List;
 @Service
 public class TeamSpService {
 
-    private final TeamSpRepository repository;
     private final TeamSpRepository teamSpRepository;
 
     @Autowired
     public TeamSpService(TeamSpRepository repository, TeamSpRepository teamSpRepository) {
-        this.repository = repository;
         this.teamSpRepository = teamSpRepository;
     }
 
     // 팀스페이스 생성
     public TeamSp saveTeamSp(TeamSp teamSp) {
-        return repository.save(teamSp);
+        return teamSpRepository.save(teamSp);
     }
 
     // 모든 팀스페이스 Entity 조회
     public List<TeamSp> getAllTeams() {
-        return repository.findAll();
+        return teamSpRepository.findAll();
     }
 
     // 특정 id 팀스페이스 조회
     public TeamSp getTeamById(long id) {
-        return repository.findById(id).orElse(null);
+        return teamSpRepository.findById(id).orElse(null);
     }
 
     // 팀스페이스 이름 수정
     public void updateTeamSp(TeamSpDto teamSpDto) {
-        TeamSp teamSp = repository.findById(teamSpDto.getTeam_id())
+        TeamSp teamSp = teamSpRepository.findById(teamSpDto.getTeam_id())
                 .orElseThrow(IllegalAccessError::new);
         teamSp.updateTeamName(teamSpDto.getTeam_name());
         teamSpRepository.save(teamSp);
@@ -45,8 +43,8 @@ public class TeamSpService {
 
     // 팀스페이스 삭제
     public void deleteTeamSp(long id) {
-        if (repository.existsById(id)) {
-            repository.deleteById(id);
+        if (teamSpRepository.existsById(id)) {
+            teamSpRepository.deleteById(id);
         }
     }
 }
