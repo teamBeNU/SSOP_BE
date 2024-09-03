9dbb6495c030e82cdd8f6e573d1fea794ce07975
mingyeong0210 <mica7150@naver.com>
2024-09-03 22:27:44 +0900

    refactor: 불필요 주석 제거

--- src/main/java/SSOP/ssop/controller/KakaoController.java
+++ src/main/java/SSOP/ssop/controller/KakaoController.java
@@ -6,7 +6,6 @@ import SSOP.ssop.service.KakaoService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +14,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,99 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-//@RestController
-//@RequestMapping("/login/kakao")
-//public class KakaoController {
-//
-//    private static final Logger log = LoggerFactory.getLogger(KakaoController.class);
-//    @Value("${kakao.client.id}")
-//    private String clientId;
-//
-//    private final KakaoService kakaoService;
-//
-//    @Autowired
-//    public KakaoController(KakaoService kakaoService) {
-//        this.kakaoService = kakaoService;
-//    }
-//
-//    @GetMapping
-//    public @ResponseBody String kakaoCallback(String code) throws JsonProcessingException {
-//
-//        RestTemplate rt = new RestTemplate();
-//
-//        //HttpHeader 오브젝트 생성
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
-//
-//        //HttpBody 오브젝트 생성
-//        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-//        params.add("grant_type", "authorization_code");
-//        params.add("client_id", clientId);
-//        params.add("redirect_uri", "http://localhost:8080/login/kakao");
-//        params.add("code", code);
-//
-//        //HttpHeader와 HttpBody를 하나의 오브젝트에 담기
-//        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
-//
-//        //Http 요청하기 - post방식, 그리고 response변수의 응답 받음
-//        ResponseEntity<String> response = rt.exchange("https://kauth.kakao.com/oauth/token", HttpMethod.POST, kakaoTokenRequest, String.class);
-//
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        OAuthToken oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
-//
-//        System.out.println("카카오 액세스 토큰 : " + oauthToken.getAccess_token());
-//
-//        // 사용자 정보 가져오기
-//        HttpHeaders userInfoHeaders = new HttpHeaders();
-//        userInfoHeaders.add("Authorization", "Bearer " + oauthToken.getAccess_token());
-//        userInfoHeaders.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
-//
-//        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(userInfoHeaders);
-//
-//        ResponseEntity<String> userInfoResponse = rt.exchange(
-//                "https://kapi.kakao.com/v2/user/me",
-//                HttpMethod.POST,
-//                kakaoProfileRequest,
-//                String.class
-//        );
-//
-//        // 사용자 정보 출력
-////        String userInfoResponseBody = userInfoResponse.getBody();
-////        System.out.println("사용자 정보: " + userInfoResponseBody);
-////
-////        // 사용자 정보를 파싱해서 활용 (예: 사용자 이름, 이메일 등)
-////        JsonNode userInfo = objectMapper.readTree(userInfoResponseBody);
-////        String kakaoId = userInfo.get("id").asText();
-////        String email = userInfo.path("kakao_account").path("email").asText();
-////        String name = userInfo.path("kakao_account").path("name").asText();
-////        String phone = userInfo.path("kakao_account").path("phone_number").asText();
-//
-////        System.out.println("유저정보: " + userInfo );
-//
-//
-//        String userInfoResponseBody = userInfoResponse.getBody();
-//        ObjectMapper mapper = new ObjectMapper();
-//        JsonNode jsonNode = mapper.readTree(userInfoResponseBody);
-//
-//        String birthyear = jsonNode.get("kakao_account").get("birthyear").asText();
-//        String birthday = jsonNode.get("kakao_account").get("birthday").asText();
-//
-//        KakaoUser kakaoUserInfo = new KakaoUser();
-//        kakaoUserInfo.setId(jsonNode.get("id").asLong());
-//        kakaoUserInfo.setName(jsonNode.get("properties").get("nickname").asText());
-//        kakaoUserInfo.setEmail(jsonNode.get("kakao_account").get("email").asText());
-//        kakaoUserInfo.setBirthyear(birthyear);
-//        kakaoUserInfo.setBirthday(birthday);
-//
-//        return "카카오 로그인 성공\n" +
-//                "이름: " + kakaoUserInfo.getName() +
-//                "\n이메일: " + kakaoUserInfo.getEmail() +
-//                "\n아이디: " + kakaoUserInfo.getId();
-//
-////        return "카카오 로그인 성공, 이름: " + name + ", 이메일: " + email + ", 번호: " + phone;
-//    }
-//}
 
 @RestController
 @RequestMapping("/login/kakao")
