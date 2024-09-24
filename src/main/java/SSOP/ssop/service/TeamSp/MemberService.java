88a59b5ff500ab160bd8f03b45a5090eb051e4ca
HJ2828 <tjgmlwo0228@gmail.com>
2024-09-24 22:53:23 +0900

    chosre: 불필요한 코드 제거, import 추가

--- src/main/java/SSOP/ssop/service/TeamSp/MemberService.java
+++ src/main/java/SSOP/ssop/service/TeamSp/MemberService.java
@@ -17,15 +17,12 @@ import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
