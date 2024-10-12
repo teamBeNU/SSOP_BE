c27abb7d34f4f13720b9765c1d91046a97448b30
yyyujinnn <1209yujin@naver.com>
2024-10-13 02:22:02 +0900

    fix: 필터링 한 번에 요청

--- src/main/java/SSOP/ssop/service/TeamSp/FilterService.java
+++ src/main/java/SSOP/ssop/service/TeamSp/FilterService.java
@@ -1,7 +1,7 @@
 package SSOP.ssop.service.TeamSp;
 
 import SSOP.ssop.domain.TeamSp.Member;
-import SSOP.ssop.dto.TeamSp.FilterDto;
+import SSOP.ssop.dto.TeamSp.FilterMemberDto;
 import SSOP.ssop.repository.TeamSp.FilterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +15,15 @@ public class FilterService {
     @Autowired
     private FilterRepository filterRepository;
 
+    // 전체 필터링
+    public List<FilterMemberDto> getMembersByFilters(Long teamId, List<String> mbti, List<String> role, List<String> major, List<String> template) {
+        List<Member> members = filterRepository.findAllByFilters(teamId, mbti, role, major, template);
+
+        return members.stream()
+                .map(FilterMemberDto::new)
+                .collect(Collectors.toList());
+    }
+
     // MBTI 목록 받아오기
     public List<String> findDistinctCardMBTIByTeamId(Long teamId) {
 
@@ -24,14 +33,6 @@ public class FilterService {
         // MBTI 값이 없는 항목 제거
         return mbtiList.stream()
                 .filter(mbti -> mbti != null && !mbti.trim().isEmpty())
-                .collect(Collectors.toList());
-    }
-
-    // MBTI별 멤버 필터링
-    public List<FilterDto> getMembersByMBTI(Long teamId, String mbti) {
-        List<Member> filter = filterRepository.findByCardMBTIAndTeamId(mbti, teamId);
-        return filter.stream()
-                .map(FilterDto::new)
                 .collect(Collectors.toList());
     }
 
@@ -47,14 +48,6 @@ public class FilterService {
                 .collect(Collectors.toList());
     }
 
-    // 역할별 멤버 필터링
-    public List<FilterDto> getMembersByRole(Long teamId, String role) {
-        List<Member> filter = filterRepository.findByCardRoleAndTeamId(role, teamId);
-        return filter.stream()
-                .map(FilterDto::new)
-                .collect(Collectors.toList());
-    }
-
     // Major 목록 받아오기
     public List<String> findDistinctCardMajorByTeamId(Long teamId) {
 
@@ -67,14 +60,6 @@ public class FilterService {
                 .collect(Collectors.toList());
     }
 
-    // 전공별 멤버 필터링
-    public List<FilterDto> getMembersByMajor(Long teamId, String major) {
-        List<Member> filter = filterRepository.findByCardMajorAndTeamId(major, teamId);
-        return filter.stream()
-                .map(FilterDto::new)
-                .collect(Collectors.toList());
-    }
-
     // Template 목록 받아오기
     public List<String> findDistinctCardTemplateByTeamId(Long teamId) {
 
@@ -84,14 +69,6 @@ public class FilterService {
         // MBTI 값이 없는 항목 제거
         return templateList.stream()
                 .filter(template -> template != null && !template.trim().isEmpty())
-                .collect(Collectors.toList());
-    }
-
-    // 템플릿별 멤버 필터링
-    public List<FilterDto> getMembersByTemplate(Long teamId, String template) {
-        List<Member> filter = filterRepository.findByCardTemplateAndTeamId(template, teamId);
-        return filter.stream()
-                .map(FilterDto::new)
                 .collect(Collectors.toList());
     }
 }
