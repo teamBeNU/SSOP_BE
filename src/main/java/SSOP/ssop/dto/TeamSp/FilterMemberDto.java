c65bf79cf0f6ecf3fa4bd35272c86b22374d3d5d
yyyujinnn <1209yujin@naver.com>
2024-10-22 01:40:27 +0900

    fix: 필터링 했을 때 카드 변수 수정

--- src/main/java/SSOP/ssop/dto/TeamSp/FilterMemberDto.java
+++ src/main/java/SSOP/ssop/dto/TeamSp/FilterMemberDto.java
@@ -9,19 +9,21 @@ import lombok.Setter;
 public class FilterMemberDto {
     private Long teamId;
     private Long cardId;
-    private String team_name;
-    private String team_comment;
+    private String card_name;
+    private String card_comment;
     private String card_birth;
     private String template;
     private String profile_image_url;
+//    private String avatar;
 
     public FilterMemberDto(Member member) {
         this.teamId = member.getTeamSpMember().getTeamSp().getTeamId();
         this.cardId = member.getCardId();
-        this.team_name = member.getCard_name();
-        this.team_comment = member.getCard_introduction();
+        this.card_name = member.getCard_name();
+        this.card_comment = member.getCard_introduction();
         this.card_birth = member.getCard_birth();
         this.template = member.getCard_template();
         this.profile_image_url = member.getProfile_image_url();
+//        this.avatar = member.
     }
 }
