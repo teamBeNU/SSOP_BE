6813733a3eb54adcc72cbdfacf0317a10b4b8808
HJ2828 <tjgmlwo0228@gmail.com>
2024-11-15 16:29:11 +0900

    fix: 아바타 테이블 수정(악세사리)

--- src/main/java/SSOP/ssop/dto/card/request/AvatarRequest.java
+++ src/main/java/SSOP/ssop/dto/card/request/AvatarRequest.java
@@ -16,7 +16,11 @@ public class AvatarRequest {
     private int hairFrontColor;  // 헤어 - 앞머리 컬러
     private int hairBackColor;  // 헤어 - 뒷머리 컬러
     private int clothes;    // 옷
-    private int acc;        // 악세사리
+    private int accEar;     // 악세사리 - 귀걸이
+    private int accNose;     // 악세사리 - 코 피어싱
+    private int accGlasses;     // 악세사리 - 안경
+    private int accPin;     // 악세사리 - 머리핀
+    private int accEtc;     // 악세사리 - 기타
     private int bg;         // 배경
     private int bgColor;    // 배경색
 }
